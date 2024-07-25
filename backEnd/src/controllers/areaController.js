bf35ba5aff2274450680a519aa4ebf296fcc944c
CayoWalterJulian <catojulian15@gmail.com>
2024-07-24 23:21:41 -0300

    se agregaron rutas para area

--- backEnd/src/controllers/areaController.js
+++ backEnd/src/controllers/areaController.js
@@ -1,13 +1,37 @@
 const path = require('path');
 const db = require('./../database/models');
-const { Association } = require('sequelize');
+
 
 
 const areaController = {
-    "list": (req, res) => {
+    list: (req, res) => {
         db.Area.findAll()
             .then(area=> {
                 res.render('area.ejs', {area})
+            })
+    },
+    create: (req ,res) =>{
+        db.Area.findAll()
+            .then(area=> {
+                res.render('createArea.ejs')
+            })
+    },
+    save: (req ,res) =>{
+        db.Area.findAll()
+            .then(area=> {
+                res.render('createArea.ejs')
+            })/*////////*/
+    },
+    edit: (req ,res) =>{
+        db.Area.findAll()
+            .then(area=> {
+                res.render('createArea.ejs')
+            })
+    },
+    delete: (req ,res) =>{
+        db.Area.findAll()
+            .then(area=> {
+                res.render('createArea.ejs')
             })
     }
 }
--- backEnd/src/routes/areaRoutes.js
+++ backEnd/src/routes/areaRoutes.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const areaController = require('../controllers/areaController');
 
 router.get('/', areaController.list);
+router.get("/create", areaController.create)
+router.get("/edit", areaController.edit)
+router.get("/delete", areaController.delete)
+
+router.post("/create", areaController.save)
+
 
 module.exports = router;
