db8ba7b595c0bfc04d3f4f7b1038cdc0cdea6965
Carla Vidal <carlamv221@gmail.com>
2024-07-26 16:26:57 -0300

    cambios en la asociacion de aspirante y experiencia

--- backEnd/src/controllers/areaController.js
+++ backEnd/src/controllers/areaController.js
@@ -1,15 +1,43 @@
 const path = require('path');
 const db = require('./../database/models');
-const { Association } = require('sequelize');
+const { where } = require('sequelize');
+
 
 
 const areaController = {
-    "list": (req, res) => {
+    list: (req, res) => {
         db.Area.findAll()
             .then(area=> {
                 res.render('area.ejs', {area})
             })
-    }
-}
+    },
+    create: (req ,res) =>{
+        db.Area.findAll()
+            .then(area=> {
+                res.render('createArea.ejs')
+            })
+    },
+    save: (req ,res) =>{
+        db.Area.create({
+            name_area: req.body.area,
+            dni_aspirant: req.body.dni
+        })
+        res.redirect ("/area")
+    },
+    edit: (req ,res) =>{
+        db.Area.findByPk(req.body.id)
+            .then(area=> {
+                res.render('editArea.ejs', {area : area})
+            })
 
+    },
+    delete: (req ,res) =>{
+        db.Area.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        res.redirect ("/area")
+        }
+    }
 module.exports = areaController;
--- backEnd/src/routes/areaRoutes.js
+++ backEnd/src/routes/areaRoutes.js
@@ -4,4 +4,12 @@ const areaController = require('../controllers/areaController');
 
 router.get('/', areaController.list);
 
+router.post("/create", areaController.save)
+router.get("/create", areaController.create)
+
+
+router.get("/edit/:id", areaController.edit)
+
+router.post("/delete/:id", areaController.delete)
+
 module.exports = router;
